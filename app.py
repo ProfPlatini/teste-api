b61de11f900219297cc3e27f5429762169b7e648
ProfPlatini <platiniluiz@outlook.com>
2026-06-26 18:17:07 -0300

    feat: adiciona conexão com banco de dados

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from agno.models.openai import OpenAIChat
 from agno.agent import Agent
 from dotenv import load_dotenv
+import os #Os permite ler as variáveis do .evn
+from supabase import create_client #Função que permite conectar no banco
 
 #Lendo a nossa API KEY que está no .env
 load_dotenv()
@@ -10,6 +12,12 @@ load_dotenv()
 app = Flask(__name__)
 #Permitindo acessos externos
 CORS(app)
+
+#Pegar o endereço e a chave do banco que está no .env
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+                         
+supabase = create_client(supabase_url,supabase_key) #Para criar a conexão com o banco, informo a URL e a KEY que estão armazenadas no .env.
 
 agente = Agent(
     model=OpenAIChat(id="gpt-4o-mini"),
@@ -32,6 +40,24 @@ def enviarPergunta():
     pergunta = dados['pergunta']
     resposta = agente.run(pergunta)
     return jsonify({"mensagem":resposta.content})
+
+#Criar método para receber a reserva e salvar no banco de dados
+@app.route("/reservar", methods=['POST'])
+def reservar():
+    dados = request.get_json()
+    
+    #Agora, preciso organizar esses dados em um dicionário, com as mesmas colunas da tabela do banco de dados
+    nova_reserva = {
+        "nome":dados["nome"],
+        "email":dados["email"],
+        "tipo_quarto":dados["tipo_quarto"],
+        "checkin":dados["checkin"]
+    }
+    
+    #Agora, comando para reservar os dados na tabela do banco
+    supabase.table("reservas").insert(nova_reserva).execute()
+    
+    return jsonify({"mensagem":"Reserva realizada com sucesso!"})
 
 #Iniciando o nosso app
 if __name__ == '__main__':
